835946e1de4770e77d1d1bbe97c70bb2adb4c453
Rodolfo Iannazzo <iannazzo@Macbook-Pro-de-Rodolfo.local>
2024-05-07 18:11:27 -0300

    adicionando novas colunas no modelo do Projeto

---
+++ src/TalentHub/Migrations/20240507190431_alteracaoProjeto.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TalentHub.Migrations
+{
+    /// <inheritdoc />
+    public partial class alteracaoProjeto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IdAnotacao",
+                table: "Anotacoes",
+                newName: "Id");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Ano",
+                table: "Projetos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Categoria",
+                table: "Projetos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DescricaoProjeto",
+                table: "Projetos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PalavraChave",
+                table: "Projetos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Periodo",
+                table: "Projetos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Ano",
+                table: "Projetos");
+
+            migrationBuilder.DropColumn(
+                name: "Categoria",
+                table: "Projetos");
+
+            migrationBuilder.DropColumn(
+                name: "DescricaoProjeto",
+                table: "Projetos");
+
+            migrationBuilder.DropColumn(
+                name: "PalavraChave",
+                table: "Projetos");
+
+            migrationBuilder.DropColumn(
+                name: "Periodo",
+                table: "Projetos");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Anotacoes",
+                newName: "IdAnotacao");
+        }
+    }
+}
